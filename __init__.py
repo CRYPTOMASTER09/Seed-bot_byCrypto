7ccf7d132d66aa4bb1b6f8518789d12be6d041e6
CRYPTOMASTER09 <technicalakmal225@gmail.com>
2024-11-22 20:51:14 +0500

    __init__.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,8 @@
-class InvalidSession(BaseException):
-    ...
+from .logger import logger
+from . import launcher
+
+
+import os
+
+if not os.path.exists(path="sessions"):
+    os.mkdir(path="sessions")
